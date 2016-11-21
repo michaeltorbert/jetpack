michaeltorbert/jetpack

commit f4ef1f8469580e2c3fff4a020f4dadb4e8f36257

Fix Carousel not starting for single image after its link was edited
Before the fix Carousel for single images was dependent on attribute rel
of 'a' tag wrapping the 'img' tag. This attribute is set differently when
image is added to post and when its link is edited in Media Settings modal.

After the fix this attribute is not used - additional data-permalink
attribute was introduced to img tag that allows easy diffrentiation
whether image links to 'Media File' or 'Attachment Page'

Fixes: #5431

modules/carousel/jetpack-carousel.js
@@ -1419,17 +1419,30 @@ jQuery(document).ready(function($) {
 	// handle lightbox (single image gallery) for images linking to 'Attachment Page'
 	if ( 1 === Number( jetpackCarouselStrings.single_image_gallery ) ) {
 		// process links that have rel=attachment and img tag inside
-		$( 'a[rel*="attachment"] img' ).each(function() {
+		$( 'a img[data-attachment-id]' ).each(function() {
 			var container = $( this ).parent();
-			// if link points to 'Media File' and flag is not set skip it
+			var valid = false;
+
+			// if link points to 'Media File' and flag is set allow it
 			if ( $( container ).attr( 'href' ) === $( this ).attr( 'data-orig-file' ) &&
-				0 === Number( jetpackCarouselStrings.single_image_gallery_media_file )
+				1 === Number( jetpackCarouselStrings.single_image_gallery_media_file )
 			) {
+				valid = true;
+			}
+
+			// if link points to 'Attachment Page' allow it
+			if( $( container ).attr( 'href' ) === $( this ).attr( 'data-permalink' ) ) {
+				valid = true;
+			}
+
+			// links to 'Custom URL' or 'Media File' when flag not set are not valid
+			if( ! valid ) {
 				return;
 			}
+
 			// make this node a gallery recognizable by event listener above
 			$( container ).addClass( 'single-image-gallery') ;
-			// method 'open' needs this attribute to distinguish from defailt WP gallery
+			// method 'open' needs this attribute to distinguish from default WP gallery
 			container.data( 'carousel-extra', { single_image: true } );
 		});
 	}

modules/carousel/jetpack-carousel.php
@@ -421,6 +421,7 @@ class Jetpack_Carousel {
 		$img_meta = json_encode( array_map( 'strval', $img_meta ) );
 
 		$attr['data-attachment-id']     = $attachment_id;
+		$attr['data-permalink']         = esc_attr( get_permalink( $attachment->ID ) );
 		$attr['data-orig-file']         = esc_attr( $orig_file );
 		$attr['data-orig-size']         = $size;
 		$attr['data-comments-opened']   = $comments_opened;

commit 68310108c179f94b26806c51cbe2f573f3fd62b3

Fix bug where images belonging to gallery were processed twice
Bug was introduced in f4ef1f8469580e2c3fff4a020f4dadb4e8f36257

Fixes: #5431

modules/carousel/jetpack-carousel.js
@@ -1425,6 +1425,12 @@ jQuery(document).ready(function($) {
 		// process links that contain img tag with attribute data-attachment-id
 		$( 'a img[data-attachment-id]' ).each(function() {
 			var container = $( this ).parent();
+
+			// skip if image was already added to gallery by shortcode
+			if( container.parent( '.gallery-icon' ).length ) {
+				return;
+			}
+
 			var valid = false;
 
 			// if link points to 'Media File' and flag is set allow it